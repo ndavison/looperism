ee38d5f2d973b219548b3e515f1dc7dd9e4a0af8
Nathan Davison <ndavison85@gmail.com>
2015-11-10 20:34:05 +1100

    Moved the config global var to lib.

---
+++ client/lib/globals.js
@@ -0,0 +1,8 @@
+/**
+ * Create the app config.
+ */
+config = {};
+config.siteRoot = '';    
+config.dropBoxDir = 'looper-audio';
+config.dropboxKey = 'hwa4du8r8utx5fa';
+config.oAuthReceiverURL = 'https://' + window.location.hostname + config.siteRoot + '/dropbox-receiver.html';
--- client/startup.js
+++ client/startup.js
@@ -1,29 +1,21 @@
 /**
- * The client side Meteor.startup() execution.
+ * The client side startup.
  */
-
 Meteor.startup(function () {
 
-    var config = {};
-    config.siteRoot = '';    
-    config.dropBoxDir = 'looper-audio';
-    config.dropboxKey = 'zr16qymzqg21hzf';
-    config.oAuthReceiverURL = 'https://' + window.location.hostname + config.siteRoot + '/dropbox-receiver.html';
-    Session.set('config', config);
-    
     /**
      * Attempt non-interactive authentication on startup.
      */
-    DropboxClient = new Dropbox.Client({key: config.dropboxKey});
-    DropboxClient.authDriver(new Dropbox.AuthDriver.Popup(
+    dropboxClient = new Dropbox.Client({key: config.dropboxKey});
+    dropboxClient.authDriver(new Dropbox.AuthDriver.Popup(
         {receiverUrl: config.oAuthReceiverURL})
     );
-    DropboxClient.authenticate({interactive: false}, function(error, client) {
-        console.log(error);
+    dropboxClient.authenticate({interactive: false}, function(error, client) {
+        if (error) console.log(error);
         if (client.isAuthenticated()) {
             Session.set('isAuthenticated', true);
-            DropboxClient.getUserInfo(function(error, userInfo) {
-                console.log(error);
+            dropboxClient.getUserInfo(function(error, userInfo) {
+                if (error) console.log(error);
                 if (userInfo) {
                     Session.set('userInfo', userInfo);
                 }
